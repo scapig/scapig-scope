d0bcbbeb2519d18c2aeec3732ac9e25b723a33cb
tomvdv59 <t.vandevelde@yahoo.fr>
2017-10-16 21:17:00 +0100

    it database

--- component/tapi/BaseFeatureSpec.scala
+++ component/tapi/BaseFeatureSpec.scala
@@ -7,9 +7,11 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import org.scalatest._
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 import repository.ScopeRepository
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await.result
 import scala.concurrent.duration.Duration
 
@@ -27,6 +29,10 @@ with GivenWhenThen with BeforeAndAfterEach with BeforeAndAfterAll with GuiceOneS
 
   val timeout = Duration(5, TimeUnit.SECONDS)
   val mocks = Seq[MockHost]()
+
+  implicit override lazy val app: Application =  new GuiceApplicationBuilder().configure(
+    "mongodb.uri" -> "mongodb://localhost:27017/tapi-api-scope-it"
+  ).build()
 
   private def mongoRepository = {
     fakeApplication.injector.instanceOf[ScopeRepository].repository
